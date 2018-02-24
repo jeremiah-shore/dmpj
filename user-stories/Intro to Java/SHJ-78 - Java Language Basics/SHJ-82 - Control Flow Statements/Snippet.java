958c8fd0f2b972cea3de08d9c59120696f8ffee6
Jeremiah Shore <shore.jeremiah@gmail.com>
2018-02-24 10:51:18 -0700

    Control Flow Statements exercises - 1d

--- user-stories/Intro to Java/SHJ-78 - Java Language Basics/SHJ-82 - Control Flow Statements/Snippet.java
+++ user-stories/Intro to Java/SHJ-78 - Java Language Basics/SHJ-82 - Control Flow Statements/Snippet.java
@@ -3,11 +3,13 @@ class Snippet {
 	public static void main(String[] args) {
 		int aNumber = 3;
 
-		if (aNumber >= 0)
-		    if (aNumber == 0)
+		if (aNumber >= 0) {
+		    if (aNumber == 0) {
 		        System.out.println("first string");
-		else 
+		    }
+		} else {
 			System.out.println("second string");
+		}
 
 		System.out.println("third string");
 	}
