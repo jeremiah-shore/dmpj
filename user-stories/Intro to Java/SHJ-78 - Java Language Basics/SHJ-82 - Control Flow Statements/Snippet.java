a543e696ca57f21a7ba8994efa56b52e969f0966
Jeremiah Shore <shore.jeremiah@gmail.com>
2018-02-24 10:49:59 -0700

    Control Flow Statements exercises - 1a and 1b

--- user-stories/Intro to Java/SHJ-78 - Java Language Basics/SHJ-82 - Control Flow Statements/Snippet.java
+++ user-stories/Intro to Java/SHJ-78 - Java Language Basics/SHJ-82 - Control Flow Statements/Snippet.java
@@ -1,11 +1,27 @@
-class Snippet {
+class Snippet { 
 
 	public static void main(String[] args) {
+		int aNumber = 3;
+
 		if (aNumber >= 0)
 		    if (aNumber == 0)
 		        System.out.println("first string");
 		else System.out.println("second string");
 		System.out.println("third string");
 	}
+
+	/*answer to 1a: 
+		second string
+		third string
+	*/
+
+	/*answer to 1b:
+		The output matches what I predicted in my answer to 1a.
+		
+		This is because the aNumber >= 0 expression evaluates to false in the first condition. The second if 
+		statement is nested and is never checked; once the first if evaluates to false the else statement is 
+		executed, printing out "second string". The statement that prints "third string" has no conditions and 
+		will always execute.
+	*/
 
 }
