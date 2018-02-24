158850bf814601feba4258d2931e84dc2b662e6f
Jeremiah Shore <shore.jeremiah@gmail.com>
2018-02-24 10:44:19 -0700

    Control Flow Statements exercises - start

---
+++ user-stories/Intro to Java/SHJ-78 - Java Language Basics/SHJ-82 - Control Flow Statements/Snippet.java
@@ -0,0 +1,11 @@
+class Snippet {
+
+	public static void main(String[] args) {
+		if (aNumber >= 0)
+		    if (aNumber == 0)
+		        System.out.println("first string");
+		else System.out.println("second string");
+		System.out.println("third string");
+	}
+
+}
