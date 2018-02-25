81ec5401cc352399bf6e376b512ac4c93cb1f649
Jeremiah Shore <shore.jeremiah@gmail.com>
2018-02-25 09:54:30 -0700

    object exercise 2 - complete

--- user-stories/Intro to Java/SHJ-84 - Java Classes, Objects, and More On Classes/NumberHolderTest.java
+++ user-stories/Intro to Java/SHJ-84 - Java Classes, Objects, and More On Classes/NumberHolderTest.java
@@ -1,3 +1,12 @@
 class NumberHolderTest {
-	
+
+	public static void main(String[] args) {
+		NumberHolder numHold = new NumberHolder();
+
+		numHold.anInt = 1;
+		numHold.aFloat = 1.9f;	
+
+		System.out.println("anInt: " + numHold.anInt);
+		System.out.println("aFloat: " + numHold.aFloat);
+	}
 }
