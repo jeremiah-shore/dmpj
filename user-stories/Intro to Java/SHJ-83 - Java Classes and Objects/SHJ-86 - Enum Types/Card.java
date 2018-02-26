3c8c2a442c7b985ae65283c49e08bd17386f061d
Jeremiah Shore <shore.jeremiah@gmail.com>
2018-02-25 19:09:48 -0700

    enum types exercise 1

--- user-stories/Intro to Java/SHJ-83 - Java Classes and Objects/SHJ-86 - Enum Types/Card.java
+++ user-stories/Intro to Java/SHJ-83 - Java Classes and Objects/SHJ-86 - Enum Types/Card.java
@@ -1,3 +1,18 @@
 class Card {
-	
+	private enum Suit {
+		DIAMONDS, CLUBS, SPADES, HEARTS
+	}
+
+	private enum Rank {
+		ACE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN,
+		JACK, QUEEN, KING
+	}
+
+	private final Suit suit;
+	private final Rank rank;
+
+	public Card(Suit suit, Rank rank) {
+		this.suit = suit;
+		this.rank = rank;
+	}
 }
