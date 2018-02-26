a265550983aac10b7860dddd7c2f12bea9aa96f1
Jeremiah Shore <shore.jeremiah@gmail.com>
2018-02-25 17:15:41 -0700

    nested classes exercise 2b

--- user-stories/Intro to Java/SHJ-83 - Java Classes and Objects/SHJ-85 - Nested Classes/DataStructure.java
+++ user-stories/Intro to Java/SHJ-83 - Java Classes and Objects/SHJ-85 - Nested Classes/DataStructure.java
@@ -18,56 +18,36 @@ public class DataStructure {
         System.out.println();
     }
 
+    private int getSize() {
+        return SIZE;
+    }
 
-
-    public void printEven() {
-        
-        // Print out values of even indices of the array
-        DataStructureIterator iterator = this.new EvenIterator();
-        while (iterator.hasNext()) {
-            System.out.print(iterator.next() + " ");
-        }
-        System.out.println();
+    private int[] getArrayOfInts() {
+        return arrayOfInts;
     }
     
     interface DataStructureIterator extends java.util.Iterator<Integer> { } 
-
-    // Inner class implements the DataStructureIterator interface,
-    // which extends the Iterator<Integer> interface
-    
-    private class EvenIterator implements DataStructureIterator {
-        
-        // Start stepping through the array from the beginning
-        private int nextIndex = 0;
-        
-        public boolean hasNext() {
-            
-            // Check if the current element is the last in the array
-            return (nextIndex <= SIZE - 1);
-        }        
-        
-        public Integer next() {
-            
-            // Record a value of an even index of the array
-            Integer retValue = Integer.valueOf(arrayOfInts[nextIndex]);
-            
-            // Get the next even element
-            nextIndex += 2;
-            return retValue;
-        }
-
-        public void remove() {}
-    }
     
     public static void main(String s[]) {
         
-        // Fill the array with integer values and print out only
-        // values of even indices
-        DataStructure ds = new DataStructure();
-        ds.print(ds.createEvenIterator());
+        // Fill the array with integer values
+        final DataStructure ds = new DataStructure();
+
+        //print out only values of odd indices
+        ds.print(
+            new DataStructureIterator() {
+                private int nextIndex = 1;
+                public boolean hasNext() {
+                    return (nextIndex <= ds.getSize() - 1);
+                }
+                public Integer next() {
+                    Integer retValue = Integer.valueOf(ds.getArrayOfInts()[nextIndex]);
+                    nextIndex += 2;
+                    return retValue;
+                }
+                public void remove() {} //just to get the compiler to cooperate wit this example
+            }
+        );
     }
 
-    private EvenIterator createEvenIterator() {
-        return new EvenIterator();
-    }
 }
