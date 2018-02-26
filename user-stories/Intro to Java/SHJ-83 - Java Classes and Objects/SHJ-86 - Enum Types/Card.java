0dd6d64b9b41cbbb3f43f35b5d126b966dc65a5e
Jeremiah Shore <shore.jeremiah@gmail.com>
2018-02-25 19:28:14 -0700

    enum types exercise 2, plus print deck contents

--- user-stories/Intro to Java/SHJ-83 - Java Classes and Objects/SHJ-86 - Enum Types/Card.java
+++ user-stories/Intro to Java/SHJ-83 - Java Classes and Objects/SHJ-86 - Enum Types/Card.java
@@ -1,18 +1,19 @@
 class Card {
-	private enum Suit {
-		DIAMONDS, CLUBS, SPADES, HEARTS
-	}
-
-	private enum Rank {
-		ACE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN,
-		JACK, QUEEN, KING
-	}
 
 	private final Suit suit;
 	private final Rank rank;
 
-	public Card(Suit suit, Rank rank) {
+	Card(Suit suit, Rank rank) {
 		this.suit = suit;
 		this.rank = rank;
 	}
+
+	Suit getSuit() {
+		return suit;
+	}
+
+	Rank getRank() {
+		return rank;
+	}
+
 }
--- user-stories/Intro to Java/SHJ-83 - Java Classes and Objects/SHJ-86 - Enum Types/Deck.java
+++ user-stories/Intro to Java/SHJ-83 - Java Classes and Objects/SHJ-86 - Enum Types/Deck.java
@@ -1,3 +1,18 @@
+import java.util.*;
+
 class Deck {
-	
+
+	private List<Card> cards;
+
+	public Deck() {
+		cards = new ArrayList<>();
+		for(Suit suit : Suit.values())
+			for(Rank rank : Rank.values()) 
+				cards.add(new Card(suit, rank));
+	}
+
+	public List<Card> getCards() {
+		return cards;
+	}
+
 }
