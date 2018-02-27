039fa39c6e56ee05c434cfa20348db0708bc8af9
Jeremiah Shore <shore.jeremiah@gmail.com>
2018-02-27 12:35:32 -0700

    numbers exercise 1 - complete

--- user-stories/Intro to Java/SHJ-91 - Java Numbers and Strings/SHJ-92 - Numbers/MaxVariablesDemo.java
+++ user-stories/Intro to Java/SHJ-91 - Java Numbers and Strings/SHJ-92 - Numbers/MaxVariablesDemo.java
@@ -2,14 +2,14 @@ public class MaxVariablesDemo {
     public static void main(String args[]) {
  
         //integers
-        byte largestByte = Byte.MAX_VALUE;
-        short largestShort = Short.MAX_VALUE;
-        int largestInteger = Integer.MAX_VALUE;
-        long largestLong = Long.MAX_VALUE;
+        byte largestByte = Byte.MIN_VALUE;
+        short largestShort = Short.MIN_VALUE;
+        int largestInteger = Integer.MIN_VALUE;
+        long largestLong = Long.MIN_VALUE;
  
         //real numbers
-        float largestFloat = Float.MAX_VALUE;
-        double largestDouble = Double.MAX_VALUE;
+        float largestFloat = Float.MIN_VALUE;
+        double largestDouble = Double.MIN_VALUE;
  
         //Display them all.
         System.out.println("The largest byte value is "
@@ -26,4 +26,13 @@ public class MaxVariablesDemo {
         System.out.println("The largest double value is "
                            + largestDouble + ".");
     }
+
+    /* SAMPLE OUTPUT:
+      The largest byte value is -128.
+      The largest short value is -32768.
+      The largest integer value is -2147483648.
+      The largest long value is -9223372036854775808.
+      The largest float value is 1.4E-45.
+      The largest double value is 4.9E-324.
+    */
 }
