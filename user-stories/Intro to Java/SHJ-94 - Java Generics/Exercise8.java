4fed8d42cfe224d99d9f74e75a81167898b65c8e
Jeremiah Shore <shore.jeremiah@gmail.com>
2018-02-28 00:20:57 -0700

    generics exercise 8 - added test code in main() for findMax, updated findMax() to check parameterized type with Comparable

--- user-stories/Intro to Java/SHJ-94 - Java Generics/Exercise8.java
+++ user-stories/Intro to Java/SHJ-94 - Java Generics/Exercise8.java
@@ -1,9 +1,13 @@
+import java.util.*;
+
 class Exercise8 {
 	public static void main(String[] args) {
-
+		List<Integer> ints= Arrays.asList(1, 5, 10, 3, 7);
+		int max = findMax(ints);
+		System.out.printf("%d is the max element in the array%n", max);
 	}
 
-	public static <T extends Comparable> findMax(List<T> list) {
+	public static <T extends Comparable<T>> T findMax(List<T> list) {
 		T max = null;
 		for(T elem : list)
 			if(max == null || elem.compareTo(max) > 0)
