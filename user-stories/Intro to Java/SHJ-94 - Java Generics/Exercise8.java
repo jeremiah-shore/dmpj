45e11297e411b414e9d1c309e171b04d6cf61dd6
Jeremiah Shore <shore.jeremiah@gmail.com>
2018-02-28 00:25:31 -0700

    generics exercise 8 - added findMaxInRangeInclusive() and test code for it in main()

--- user-stories/Intro to Java/SHJ-94 - Java Generics/Exercise8.java
+++ user-stories/Intro to Java/SHJ-94 - Java Generics/Exercise8.java
@@ -5,6 +5,9 @@ class Exercise8 {
 		List<Integer> ints= Arrays.asList(1, 5, 10, 3, 7);
 		int max = findMax(ints);
 		System.out.printf("%d is the max element in the array%n", max);
+
+		max = findMaxInRangeInclusive(ints, 0, 1);
+		System.out.printf("%d is the max element in the array%n", max);
 	}
 
 	public static <T extends Comparable<T>> T findMax(List<T> list) {
@@ -13,5 +16,11 @@ class Exercise8 {
 			if(max == null || elem.compareTo(max) > 0)
 				max = elem;
 		return max;
+	}
+
+
+	public static <T extends Comparable<T>> T findMaxInRangeInclusive(List<T> list, int start, int end) {
+		List<T> range = list.subList(start, ++end);
+		return findMax(range);
 	}
 }
