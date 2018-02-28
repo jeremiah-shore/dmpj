18745559a7b31cb51ccfd57e36a9d3cfa83d366e
Jeremiah Shore <shore.jeremiah@gmail.com>
2018-02-27 23:34:30 -0700

    generics exercise 1 - modeled my answer after the provided answer, but used standard functional interface of Predicate<T>, and also used .negate() to test for odd numbers

--- user-stories/Intro to Java/SHJ-94 - Java Generics/Exercise1.java
+++ user-stories/Intro to Java/SHJ-94 - Java Generics/Exercise1.java
@@ -1,10 +1,28 @@
-import java.util.List;
-import java.util.ArrayList;
-import java.util.Function.*;
+import java.util.*;
+import java.util.function.*;
 
 class Exercise1 {
 	public static void main(String[] args) {
+		List<Integer> nums = Arrays.asList(1, 2, 3, 4, 5);
 
+		int count = countIf(nums, new EvenPredicate());
+		System.out.printf("There are %d even numbers in the array%n", count);
+
+		count = countIf(nums, new EvenPredicate().negate());
+		System.out.printf("There are %d odd numbers in the array%n", count);
 	}
 
+	public static <T> int countIf(Collection<T> c, Predicate<T> p) {
+		int count = 0;
+		for(T elem : c)
+			if(p.test(elem))
+				count++;
+		return count;
+	}
+
+	static class EvenPredicate implements Predicate<Integer> {
+		public boolean test(Integer i) {
+			return i % 2 == 0;
+		}
+	}
 }
