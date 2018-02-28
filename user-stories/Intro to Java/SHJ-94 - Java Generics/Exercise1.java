c379f5625b33879dd5c42a70c28bb3d51e3cb9de
Jeremiah Shore <shore.jeremiah@gmail.com>
2018-02-27 23:15:58 -0700

    generics - question/exercise 1 - built generic method after model, will use to test if a substring can be found in any class that extends CharSequence

--- user-stories/Intro to Java/SHJ-94 - Java Generics/Exercise1.java
+++ user-stories/Intro to Java/SHJ-94 - Java Generics/Exercise1.java
@@ -1,11 +1,17 @@
 import java.util.List;
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.Function.*;
 
 class Exercise1 {
 	public static void main(String[] args) {
 
 	}
 
-
+	public static <T extends CharSequence> int countIf(Collection<T> c, Predicate<T> p) {
+		int count = 0;
+		for(T elem : c)
+			if(p.test(elem))
+				count++;
+		return count;
+	}
 }
